20b165cc508bd7602271c933227da538b6d30a4d
DrewGithubS <62274483+DrewGithubS@users.noreply.github.com>
2020-03-16 21:08:29 -0700

    Create newAI.py

---
+++ newAI.py
@@ -0,0 +1,16 @@
+import neuralNetwork
+ai = neuralNetwork.nueral_network(2,[2,2],2) # Initializes a new neural network with 4 inputs, 3 hidden layers of 7, 5 and 5 neurons and 2 outputs
+output = []
+for _ in range(1000): 
+    ai.set_values([5,5]) # Uses random weights and biases to get a result, The True parameter prints the network.
+    output = ai.feed_forward() # ai.optimize(expected_outputs, print_bool) # this would 'tune' the weights and biases to make the next guess hopefully closer to the expected_output
+    ai.backpropgate([0.7, 0.1]) # Optimizes the network given the amount of outputs
+    ai.reset_values() # Sets all values of the neurons to 0
+
+
+
+ai.set_values([5,5]) # Uses random weights and biases to get a result, The True parameter prints the network.
+output = ai.feed_forward() # ai.optimize(expected_outputs, print_bool) # this would 'tune' the weights and biases to make the next guess hopefully closer to the expected_output
+ai.print_network() # Prints all neuron information such as weight, bias, and value
+for (output_count, output_value) in enumerate(output):
+    print("Output {}: {}".format(output_count+1, output_value)) # Prints the outputs
